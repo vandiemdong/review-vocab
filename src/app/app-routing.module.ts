5af5c607cd2aa2e4f0dc1219b8788fe09c75fa91
asp31 <asp31@INTEGRATION-WIN11>
2026-05-25 17:48:32 +0800

    update UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LessonsComponent } from './pages/lessons/lessons.component';
+import { FlashcardComponent } from './pages/flashcard/flashcard.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { ReviewComponent } from './pages/review/review.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hsk/:hsk/lessons', component: LessonsComponent },
+  { path: 'hsk/:hsk/lesson/:lesson/flashcard', component: FlashcardComponent },
+  { path: 'hsk/:hsk/lesson/:lesson/quiz', component: QuizComponent },
+  { path: 'review', component: ReviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LessonsComponent } from './pages/lessons/lessons.component';
+import { FlashcardComponent } from './pages/flashcard/flashcard.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { WordCardComponent } from './components/word-card/word-card.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LessonsComponent,
+    FlashcardComponent,
+    QuizComponent,
+    ReviewComponent,
+    WordCardComponent,
+    ProgressBarComponent,
+    BottomNavComponent
   ],
   imports: [
     BrowserModule,
